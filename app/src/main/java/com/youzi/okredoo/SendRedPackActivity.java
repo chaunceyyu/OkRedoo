b7df82b090def7c4c63038c60acfd5aa36d05fb9
zhangjiajie <jackzjj@qq.com>
2017-10-10 16:27:02 +0800

    no message

---
+++ app/src/main/java/com/youzi/okredoo/SendRedPackActivity.java
@@ -0,0 +1,17 @@
+package com.youzi.okredoo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by zhangjiajie on 2017/10/10.
+ */
+
+public class SendRedPackActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.send_pack_activity);
+    }
+}
---
+++ app/src/main/java/com/youzi/okredoo/StatisticsActivity.java
@@ -0,0 +1,18 @@
+package com.youzi.okredoo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * 统计界面
+ * Created by zhangjiajie on 2017/10/10.
+ */
+
+public class StatisticsActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.statistics_activity);
+    }
+}
